6aafdc14801a918733f69aefb9051910f563ab31
Codeurge123 <bansalyash316@gmail.com>
2025-12-26 18:14:29 +0530

    Just a temperary update

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -51,13 +51,11 @@ function CardWithLabel({ label, theme = "amber", children }) {
   );
 }
 
-/* ---------------------------------------
-   LeetCode Page
---------------------------------------- */
+
 export default function Leetcode() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const username = "code_urgyb316_";
+  const username = "prathamsharda";
 
   useEffect(() => {
     const fetchStats = async () => {
